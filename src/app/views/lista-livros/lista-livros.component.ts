4a83babc208b2958faad0fc16d16beaed73ac939
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-07-13 13:36:16 -0300

    Added switchMap, pipe async, filter and debounceTime operators

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,34 +1,33 @@
-import { Item, Livro } from './../../models/interfaces';
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { Item } from './../../models/interfaces';
+import { Component } from '@angular/core';
+import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 300;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-  campoBusca: string = '';
-  subscription: Subscription;
-  livro: Livro;
+  campoBusca = new FormControl;
 
   constructor(
     private service: LivroService
   ) { }
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca)
-      .subscribe({
-        next: items => {
-          this.listaLivros = this.livrosResultadoParaLivros(items)
-        },
-        error: erro => console.error(erro)
-      })
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+      map(items => this.livrosResultadoParaLivros(items)),
+      tap(() => console.log('Requisição ao servidor'))
+    )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
@@ -36,11 +35,4 @@ export class ListaLivrosComponent implements OnDestroy{
     })
   }
 
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
-  }
-
 }
-
-
-
