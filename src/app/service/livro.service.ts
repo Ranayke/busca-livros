e2c7b757b64f1f985887a02ba08e0929728df8ca
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-07-13 11:22:29 -0300

    Created pipe to use map for subscription return

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { Observable, map, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +14,12 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscar(valorDigitado: string): Observable<LivrosResultado> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado)
     return this.http.get<LivrosResultado>(this.API, { params })
+      .pipe(
+        map(resultado => resultado.items),
+        tap(resultado => console.log(resultado))
+      )
   }
 }
