154b167ea892054a3557c2bf2a15fc5d67301bab
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-07-15 14:34:45 -0300

    Created log for errors and show number of books to user

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { FormControl } from '@angular/forms';
-import { Item } from './../../models/interfaces';
+import { Item, Livro, LivrosResultado } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -14,20 +14,45 @@ const PAUSA = 300;
 export class ListaLivrosComponent {
 
   campoBusca = new FormControl;
+  mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(
     private service: LivroService
   ) { }
 
-  livrosEncontrados$ = this.campoBusca.valueChanges
+  campoBuscaChanges$ = this.campoBusca.valueChanges
     .pipe(
       debounceTime(PAUSA),
       filter((valorDigitado) => valorDigitado.length >= 3),
       tap(() => console.log('Fluxo inicial')),
-      switchMap(valorDigitado => this.service.buscar(valorDigitado)),
-      map(items => this.livrosResultadoParaLivros(items)),
-      tap(() => console.log('Requisição ao servidor'))
+      distinctUntilChanged(),
+      shareReplay(),
+      switchMap(valorDigitado => this.service.buscar(valorDigitado))
     )
+  ;
+
+  totalDeLivros$ = this.campoBuscaChanges$
+    .pipe(
+      map(resultado => this.livrosResultado = resultado),
+      catchError(erro => {
+        console.log(erro);
+        return of();
+      })
+    )
+  ;
+
+  livrosEncontrados$ = this.campoBuscaChanges$
+    .pipe(
+      map(resultado => resultado.items ?? []),
+      map(items => this.livrosResultadoParaLivros(items)),
+      tap(() => console.log('Requisição ao servidor')),
+      catchError(erro => {
+        console.log(erro);
+        throw new Error(this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação');
+      })
+    )
+  ;
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
